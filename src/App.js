3a73a80ed0b147c18b6c9a82c79e991238d1dbfc
Nathyane Moreno <nathyane.moreno@gmail.com>
2020-08-26 07:15:43 -0300

    Ajuste do Text Field na pagina, do titulo em index.html.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TextField } from '@material-ui/core'
+import { TextField , Container} from '@material-ui/core'
 import './App.css';
 
 function App() {
@@ -22,60 +22,52 @@ function App() {
     if (d.includes(lastXY)) {
       if (arr1.filter(n => n === lastXY).length < 3
       ) {
-        console.log(1)
         setInputNumber(e)
       }
       else {
-        console.log(2)
         setInputNumber(e.slice(0, -1))
       }
     }
     if (arr2.filter(n => n === last).length > 1) {
       if (last === "1" && arr2.filter(n => n === "1").length <= 8) {
         if (e.match(/11[^\D1]*1111/) || e.match(/11111/) || e.match(/1111[^\D1]*11/) || last === "0") {
-          console.log(3)
           setInputNumber(e.slice(0, -1))
         } else {
-          console.log(4)
           setInputNumber(e)
         }
       } else {
         if (arr2.filter(n => n === last).length > 1 && last.match(/[6-9]|0/)) {
-          console.log(5)
           setInputNumber(e.slice(0, -1))
         }
         if (arr2.filter(n => n === last).length > 2) {
-          console.log(6)
           setInputNumber(e.slice(0, -1))
         }
         if (arr2.filter(n => n === "1").length > 8) {
-          console.log(7)
           setInputNumber(e.slice(0, -1))
         }
       }
     } else {
       if (last === "0" && e.slice(-2, -1) !== "1") {
-        console.log(8)
         setInputNumber(e.slice(0, -1))
       } else {
-        console.log(9)
         setInputNumber(e)
       }
     }
-    console.log(arr1, arr2)
   }
 
   useEffect(() => {
   }, [inputNumber])
   return (
-    <div>
+    <Container>
       <TextField
-        fullWidth variant='filled' placeholder="Insira número" label="Número" style={{ margin: 8 }} value={inputNumber}
+        fullWidth variant='filled' placeholder="Insira número" label="Número" 
+        style={{ margin: 16 }} 
+        value={inputNumber}
         onChange={e => handleChangeInput(e.target.value)} >
 
       </TextField>
       {inputNumber}
-    </div>
+    </Container>
   );
 }
 
